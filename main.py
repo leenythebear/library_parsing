ad368f05c5456f04c8e966f5dfd05c19570b5606
leeny_the_bear <leeny@inbox.ru>
2022-09-22 17:55:13 +0700

    move the saving of the book into a separate function

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import requests
 import os
 
 
-def download_book():
-    for book_id in range(0, 9):
-        url = f'https://tululu.org/txt.php?id={book_id}'
-        response = requests.get(url)
-        response.raise_for_status()
-        os.makedirs('books', exist_ok=True)
-        with open(f'books/{book_id}.txt', 'wt', encoding='utf-8') as file:
-            file.write(response.text.replace('\xa0', ''))
+def download_book(url, book_id):
+    params = {'id': book_id}
+    response = requests.get(url, params=params, allow_redirects=True)
+    response.raise_for_status()
+    return response
+
+
+def save_book(response):
+    with open(f'books/{book_id}.txt', 'wt', encoding='utf-8') as file:
+        file.write(response.text.replace('\xa0', ''))
 
 # print(response.text)
