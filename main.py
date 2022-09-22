900f2396c27776f77f7d89631170c4761251fc26
leeny_the_bear <leeny@inbox.ru>
2022-09-22 17:58:14 +0700

    make main function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from dotenv import load_dotenv
 import os
 
 
@@ -19,5 +20,16 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
+if __name__ == "__main__":
+    load_dotenv()
+    url = os.getenv('URL')
+    for book_id in range(1, 11):
+        response = download_book(url, book_id)
+        os.makedirs('books/', exist_ok=True)
+        try:
+            check_for_redirect(response)
+            save_book(response)
+        except requests.HTTPError:
+            print("Запрашиваемвя книга не найдена")
 
 
