9c9a408d558a6317b3d0741b097e1fc5359123a3
leeny_the_bear <leeny@inbox.ru>
2022-09-26 14:15:04 +0700

    make script for parsing the page

---
+++ parse_book_page.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_book_title_author(soup):
+    title_and_author = soup.find('title').text.split(', ')[0]
+    book_name = title_and_author.split('-')[0].strip()
+    author = title_and_author.split('-')[1].strip()
+    return str(book_name + '-' + author)
+
+
+if __name__ == "__main__":
+    url = 'https://tululu.org/b32168/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    print(get_book_title_author(soup))
