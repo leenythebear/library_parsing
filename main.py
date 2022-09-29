aaaea47cebc07d786553c1c0d6a3ebe8dc7e20d5
leeny_the_bear <leeny@inbox.ru>
2022-09-29 13:02:13 +0700

    format code

--- main.py
+++ main.py
@@ -12,50 +12,63 @@ def check_for_redirect(response):
         raise requests.HTTPError
 
 
-def download_txt(book_id, filename, folder='books/'):
-    url = 'https://tululu.org/txt.php'
-    params = {'id': book_id}
+def download_txt(book_id, filename, folder="books/"):
+    url = "https://tululu.org/txt.php"
+    params = {"id": book_id}
     response = requests.get(url, params)
     response.raise_for_status()
     check_for_redirect(response)
 
-    correct_filename = f'{sanitize_filename(filename)}.txt'
+    correct_filename = f"{sanitize_filename(filename)}.txt"
     os.makedirs(folder, exist_ok=True)
     file_path = os.path.join(folder, correct_filename)
-    with open(file_path, 'wt', encoding='utf-8') as file:
-        file.write(response.text.replace('\xa0', ''))
+    with open(file_path, "wt", encoding="utf-8") as file:
+        file.write(response.text.replace("\xa0", ""))
 
 
-def download_image(image_url, folder='images/'):
-    filename = urlsplit(image_url).path.split('/')[-1]
+def download_image(image_url, folder="images/"):
+    filename = urlsplit(image_url).path.split("/")[-1]
     os.makedirs(folder, exist_ok=True)
     file_path = os.path.join(folder, filename)
     response = requests.get(image_url)
     response.raise_for_status()
-    with open(file_path, 'wb', ) as file:
+    with open(
+        file_path,
+        "wb",
+    ) as file:
         file.write(response.content)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--start_id', help='Начальный id книги для парсинга', default=1, type=int)
-    parser.add_argument('-e', '--end_id', help='Конечный id книги для парсинга', default=11, type=int)
+    parser.add_argument(
+        "-s",
+        "--start_id",
+        help="Начальный id книги для парсинга",
+        default=1,
+        type=int,
+    )
+    parser.add_argument(
+        "-e",
+        "--end_id",
+        help="Конечный id книги для парсинга",
+        default=11,
+        type=int,
+    )
     args = parser.parse_args()
     start_id = args.start_id
     end_id = args.end_id
     for id_book in range(start_id, end_id):
 
-        url = f'https://tululu.org/b{id_book}/'
+        url = f"https://tululu.org/b{id_book}/"
         try:
             response = requests.get(url)
             check_for_redirect(response)
             book_information = parse_book_page(response, id_book)
-            print(book_information['title'])
-            print(book_information['genres'])
-            download_txt(id_book, book_information['title'])
-            download_image(book_information['image_url'])
+            print(book_information["title"])
+            print(book_information["genres"])
+            download_txt(id_book, book_information["title"])
+            download_image(book_information["image_url"])
 
         except requests.HTTPError:
             print("Запрашиваемая книга не найдена")
-
-
--- parse_book_page.py
+++ parse_book_page.py
@@ -5,28 +5,28 @@ from bs4 import BeautifulSoup
 
 
 def get_book_title_author(soup):
-    title_and_author = soup.find('table', class_='tabs').find('h1').text
-    title, author = title_and_author.split('::')
+    title_and_author = soup.find("table", class_="tabs").find("h1").text
+    title, author = title_and_author.split("::")
     return title.strip(), author.strip()
 
 
 def get_book_image_url(soup):
-    url = soup.find('div', class_='bookimage').find('img')['src']
-    common_url = 'https://tululu.org/'
+    url = soup.find("div", class_="bookimage").find("img")["src"]
+    common_url = "https://tululu.org/"
     image_url = urljoin(common_url, url)
     return image_url
 
 
 def get_book_comments(soup):
-    comments_tags = soup.find_all('div', class_='texts')
+    comments_tags = soup.find_all("div", class_="texts")
     comments = []
     for comment in comments_tags:
-        comments.append(comment.find('span').text)
+        comments.append(comment.find("span").text)
     return comments
 
 
 def get_book_genre(soup):
-    genre_tags = soup.find('span', class_='d_book').find_all('a')
+    genre_tags = soup.find("span", class_="d_book").find_all("a")
     genres = []
     for genre in genre_tags:
         genres.append(genre.text)
@@ -35,18 +35,16 @@ def get_book_genre(soup):
 
 def parse_book_page(response, book_id):
     response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, "lxml")
     title, author = get_book_title_author(soup)
     image_url = get_book_image_url(soup)
     genres = get_book_genre(soup)
     comments = get_book_comments(soup)
     book_information = {
-        'title': title,
-        'author': author,
-        'image_url': image_url,
-        'genres': genres,
-        'comments': comments,
+        "title": title,
+        "author": author,
+        "image_url": image_url,
+        "genres": genres,
+        "comments": comments,
     }
     return book_information
-
-
