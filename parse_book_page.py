29827a48740e2aa3ddc21b78d8a305c5fa46d470
leeny_the_bear <leeny@inbox.ru>
2022-10-03 19:59:16 +0700

    move getting the link to the parsing function

--- parse_book_page.py
+++ parse_book_page.py
@@ -1,4 +1,4 @@
-from urllib.parse import urljoin
+
 
 from bs4 import BeautifulSoup
 
@@ -7,13 +7,6 @@ def get_book_title_author(soup):
     title_and_author = soup.find("table", class_="tabs").find("h1").text
     title, author = title_and_author.split("::")
     return title.strip(), author.strip()
-
-
-def get_book_image_url(soup):
-    url = soup.find("div", class_="bookimage").find("img")["src"]
-    common_url = "https://tululu.org/"
-    image_url = urljoin(common_url, url)
-    return image_url
 
 
 def get_book_comments(soup):
@@ -31,7 +24,7 @@ def get_book_genres(soup):
 def parse_book_page(response):
     soup = BeautifulSoup(response.text, "lxml")
     title, author = get_book_title_author(soup)
-    image_url = get_book_image_url(soup)
+    image_url = soup.find("div", class_="bookimage").find("img")["src"]
     genres = get_book_genres(soup)
     comments = get_book_comments(soup)
     book = {
