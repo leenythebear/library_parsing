13aa5998dbba906a2fff8c3710dd31744a766842
leeny_the_bear <leeny@inbox.ru>
2022-10-04 15:05:35 +0700

    change func for getting all the books url from the page

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -8,9 +8,11 @@ def get_book():
     url = 'https://tululu.org/l55/'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "lxml")
-    book_link = soup.find('table', class_='d_book').find('a')['href']
-    book_url = urljoin(url, book_link)
-    print(book_url)
+    book_tags = soup.find_all('table', class_='d_book')
+    for book_tag in book_tags:
+        book_link = book_tag.find('a')['href']
+        book_url = urljoin(url, book_link)
+        print(book_url)
 
 
 if __name__ == "__main__":
