2022fc7d58ee730490959f56f74a49453410f436
leeny_the_bear <leeny@inbox.ru>
2022-10-24 15:02:05 +0600

    add script for render website

---
+++ render_website.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import json
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+template = env.get_template('index.html')
+with open("books.json", "r") as library:
+    books_json = library.read()
+books = json.loads(books_json)
+rendered_page = template.render(books=books)
+
+# return rendered_page
+print(rendered_page)
+
+
+# with open('index.html', 'w', encoding="utf8") as file:
+#     file.write(rendered_page)
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
