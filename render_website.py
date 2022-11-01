912fd53e203d09dfcac8d789f0051342b526f94c
leeny_the_bear <leeny@inbox.ru>
2022-11-01 23:26:38 +0600

    rename variables

--- render_website.py
+++ render_website.py
@@ -13,13 +13,13 @@ NUMBER_OF_COLUMNS = 2
 def reload():
     env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template.html')
-    with open("books.json", "r") as books:
-        books_json = json.load(books)
+    with open("books.json", "r") as books_json:
+        books = json.load(books_json)
 
     folder_path = 'pages/'
     os.makedirs(folder_path, exist_ok=True)
 
-    books_for_page = list(chunked(books_json, BOOKS_PER_PAGE))
+    books_for_page = list(chunked(books, BOOKS_PER_PAGE))
     page_count = len(books_for_page)
 
     for page_number, separate_books in enumerate(books_for_page, 1):
